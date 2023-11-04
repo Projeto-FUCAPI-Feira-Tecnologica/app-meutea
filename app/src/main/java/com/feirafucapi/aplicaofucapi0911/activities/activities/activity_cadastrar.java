a4d8fe7f50d7a0d77ccc9d5e01938c36ada18151
Victor <luizcunha001123@gmail.com>
2023-11-04 17:32:07 -0400

    Banco de dados no cadastro fds

--- app/src/main/java/com/feirafucapi/aplicaofucapi0911/activities/activities/activity_cadastrar.java
+++ app/src/main/java/com/feirafucapi/aplicaofucapi0911/activities/activities/activity_cadastrar.java
@@ -2,19 +2,24 @@ package com.feirafucapi.aplicaofucapi0911.activities.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.feirafucapi.aplicaofucapi0911.R;
+import com.feirafucapi.aplicaofucapi0911.activities.helper.DBHelper;
 
 public class activity_cadastrar extends AppCompatActivity {
 
     private Button btnCadastrar;
     private ImageButton ibCad;
 
+    DBHelper dbTea = new DBHelper(this); // Criação do objeto referente ao banco de dados
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +31,34 @@ public class activity_cadastrar extends AppCompatActivity {
         btnCadastrar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                EditText campoNome = findViewById(R.id.campoNome);
+                EditText campoSenha = findViewById(R.id.campoSenha);
+                EditText campoEmail = findViewById(R.id.campoEmail);
+
+                String nome = campoNome.getText().toString();
+                String senha = campoSenha.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                SQLiteDatabase db = dbTea.getWritableDatabase();
+
+                ContentValues values = new ContentValues();
+                values.put("nome", nome);
+                values.put("senha", senha);
+                values.put("email", email);
+
+                long newRowId = db.insert("Usuario", null, values);
+
+                // Verificando se foi bem-sucedida a inserção de dados no banco
+                if(newRowId != -1) {
+                    // O que vai acontecer se foi bem-sucedido
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    // Mensagem de erro quando não for bem sucedido
+                }
+
+                db.close();
             }
         });
 
--- app/src/main/java/com/feirafucapi/aplicaofucapi0911/activities/helper/DBHelper.java
+++ app/src/main/java/com/feirafucapi/aplicaofucapi0911/activities/helper/DBHelper.java
@@ -7,26 +7,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "MeuTea.db";
+    private static final int DATABASE_VERSION = 1;
 
-    public static int VERSION = 1;
-    public static String NOME_DB = "DB_USERS";
-
-    public DBHelper(@Nullable Context context) {
-        super(context, NOME_DB, null, VERSION);
-
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-
-
+        String createTableSQL = "CREATE TABLE Usuario (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nome TEXT, " +
+                "senha TEXT, " +
+                "email TEXT)";
+        db.execSQL(createTableSQL);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-
-
+        // Se você precisar atualizar o esquema do banco de dados, adicione a lógica aqui
+        // Por exemplo, você pode executar instruções SQL para fazer alterações na estrutura da tabela
     }
 }
